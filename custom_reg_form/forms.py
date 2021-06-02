ca9c69d6fbef991d698f8ec734f1abf3d685e221
murat-polat <muratsdepo@gmail.com>
2021-06-02 16:36:42 +0200

    added country dropdown

--- custom_reg_form/forms.py
+++ custom_reg_form/forms.py
@@ -4,6 +4,10 @@ from django.utils.translation import gettext_lazy as _
 import logging
 
 
+
+
+      
+      
 class ExtraInfoForm(ModelForm):
     """
     The fields on this form are derived from the ExtraInfo model in models.py.
@@ -13,10 +17,11 @@ class ExtraInfoForm(ModelForm):
         self.fields['nationality'].required = True
         self.fields['age'].required = True
         self.fields['phone_number'].required = True
+       
         
 
     class Meta(object):
         model = ExtraInfo
-        fields = ('nationality','age','phone_number',)
+        fields = ('nationality','age','phone_number', )
         labels = {'nationality': _("Nationality"),'age': _("Age"),'phone_number': _("Phone number"),}
         help_text = {'nationality': _("Please enter your Nationality"),'age': _("Please enter your Age"),'phone_number': _("Please enter your phone number"),}
